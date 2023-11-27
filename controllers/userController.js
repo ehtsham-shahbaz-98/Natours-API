24cfe3767dd08e158499e3e87f09d84af0249263
ratroot92 <ratroot92@gmail.com>
2023-11-27 11:30:08 +0500

    Update: Eslint issues resolving

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,29 @@
-exports.getAllUsers = (req, res) => {
-  return res.status(500).json({
+exports.getAllUsers = (req, res) =>
+  res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined',
   });
-};
 
-exports.getUser = (req, res) => {
-  return res.status(500).json({
+exports.getUser = (req, res) =>
+  res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined',
   });
-};
 
-exports.createUser = (req, res) => {
-  return res.status(500).json({
+exports.createUser = (req, res) =>
+  res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined',
   });
-};
 
-exports.updateUser = (req, res) => {
-  return res.status(500).json({
+exports.updateUser = (req, res) =>
+  res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined',
   });
-};
 
-exports.deleteUser = (req, res) => {
-  return res.status(500).json({
+exports.deleteUser = (req, res) =>
+  res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined',
   });
-};
