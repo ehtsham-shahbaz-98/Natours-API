712bcab46f8f2e050504e5db5ff23daebcabcdb5
ratroot92 <ratroot92@gmail.com>
2023-12-01 01:03:44 +0500

    Update: Making the api better, filtering part done

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,21 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query;
 
     return res.status(200).json({
       status: 'success',
@@ -55,7 +69,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     return res.status(400).json({
       status: 'fail',
-      message: 'Invalid Data Sent!',
+      message: err,
     });
   }
 };
@@ -64,12 +78,13 @@ exports.updateTour = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
+      runValidators: true,
     });
 
     return res.status(200).json({
       status: 'success',
       data: {
-        tour: '<Updated tour here...>',
+        tour,
       },
     });
   } catch (err) {
@@ -80,16 +95,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  // if (req.params.id * 1 > tours.length) {
-  //   return res.status(404).json({
-  //     status: 'fail',
-  //     message: 'Invalid ID',
-  //   });
-  // }
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
 
-  return res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+    return res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    return res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
